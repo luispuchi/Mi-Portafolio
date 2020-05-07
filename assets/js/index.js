6653c185a214b5567a3a944f773fc94b56d6a2bb
luis puchi <puchiluis11@gmail.com>
2020-05-07 14:31:23 -0400

    deleted comment

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,21 +17,4 @@ $( document ).ready(function() {
     
 });
 
-// window.scroll({
-    //
-   // top: 2500, 
-   // left: 0, 
-//  behavior: 'smooth'
-//});
-
-//window.scrollBy({ 
-  //  top: 100,
-   // left: 0, 
-   // behavior: 'smooth' 
-//});
-
-///document.querySelector('#description', '#my-experience', '#my-jobs').scrollIntoView({ 
-//    behavior: 'smooth' 
-//});
-
 
