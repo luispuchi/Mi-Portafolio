f49cb8af87e2fb47fdfa0449fca87fd89db4450f
luis puchi <puchiluis11@gmail.com>
2020-05-07 14:16:32 -0400

    actualizacion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,36 @@
 $( document ).ready(function() {
     $('[data-toggle="tooltip"]').tooltip()
+
+    $(function(){
+        $("'#description', '#my-experience', '#my-jobs'").click(function(e){
+          if (this.hash !== ""){
+            e.preventDefault();
+            var url = this.hash;
+            $('html,body').animate({
+              scrollTop: $(url).offset().top
+            }, 500, function(){
+              window.location.hash = url;
+            });
+          }
+        });
+    });
 });
 
-window.scroll({
-    top: 2500, 
-    left: 0, 
-    behavior: 'smooth'
-});
+// window.scroll({
+    //
+   // top: 2500, 
+   // left: 0, 
+//  behavior: 'smooth'
+//});
 
-window.scrollBy({ 
-    top: 100,
-    left: 0, 
-    behavior: 'smooth' 
-});
+//window.scrollBy({ 
+  //  top: 100,
+   // left: 0, 
+   // behavior: 'smooth' 
+//});
 
-document.querySelector('#description', '#my-experience', '#my-jobs').scrollIntoView({ 
-    behavior: 'smooth' 
-});
+///document.querySelector('#description', '#my-experience', '#my-jobs').scrollIntoView({ 
+//    behavior: 'smooth' 
+//});
+
+
